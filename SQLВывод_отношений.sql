12f6b62226890ff0c74f5815a84b15d1eee8c2a0
AnastasiaOr <66983219+AnastasiaOr@users.noreply.github.com>
2020-06-17 11:24:08 +0700

    Update SQLВывод_отношений.sql

--- SQLВывод_отношений.sql
+++ SQLВывод_отношений.sql
@@ -2,5 +2,5 @@ Use Library
 go
 Select Book_name, CatName from Relation join Books on Books.ID_book=Relation.ID_Book 
 full outer join Category on Relation.ID_Cat=Category.id_Cat
-
+--Relation представляет таблицу соответствий определенной книги и ее категории
  
